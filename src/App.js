c4e2f415fbc4cb0d4a3df25ac19b5e0c89802c58
Sahil Vishwakarma <sahilvishwa2108@gmail.com>
2023-11-04 15:59:53 +0530

    adding react components

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Item from './components/Item';
+
+function App() {
+  return (
+    <div>
+      <Item></Item>
+      <div className="App">
+        Hello Jee
+      </div>
+    </div>  
+  );
+}
+
+export default App;
